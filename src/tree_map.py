0d42bd06b528cdfdc9d8b9870dc44f7ee4167730
Marat Mingazov <mingazov.marat2003@yandex.ru>
2022-03-23 15:28:03 +0300

    del, del tests, recursive set/get added

--- src/tree_map.py
+++ src/tree_map.py
@@ -11,43 +11,90 @@ class TreeMap:
         self.root = root
 
     def __setitem__(self, key, value):
-        curr = self.root
-        while curr is not None:
-            if key > curr.key:
-                if curr.right is None:
-                    curr.right = Node(key, value)
-                    return True
+        def set_node(node):
+            if key > node.key:
+                if node.right is None:
+                    node.right = Node(key, value)
                 else:
-                    curr = curr.right
-            elif key < curr.key:
-                if curr.left is None:
-                    curr.left = Node(key, value)
-                    return True
+                    set_node(node.right)
+            elif key < node.key:
+                if node.left is None:
+                    node.left = Node(key, value)
                 else:
-                    curr = curr.left
-            else:
-                curr.value = value
-                return True
-        self.root = Node(key, value)
+                    set_node(node.left)
+            elif key == node.key:
+                node.value = value
+
+        if self.root is None:
+            self.root = Node(key, value)
+        else:
+            set_node(self.root)
 
     def __getitem__(self, key):
-        curr = self.root
-        while curr is not None and key != curr.key:
-            if key > curr.key:
-                curr = curr.right
-            else:
-                curr = curr.left
-        if curr is None:
-            raise KeyError('Such key does not exist')
-        else:
-            return curr.value
+        def get_node(node):
+            if node is None:
+                raise KeyError('Such key does not exist')
+            if key == node.key:
+                return node
+            if key > node.key:
+                return get_node(node.right)
+            if key < node.key:
+                return get_node(node.left)
+        return get_node(self.root).value
+
+    def __delitem__(self, key):
+        def del_node(node, key_, prev_node=None):
+            if node is None:
+                raise KeyError('Such key does not exist')
+            elif key_ > node.key:
+                del_node(node.right, key_, node)
+            elif key_ < node.key:
+                del_node(node.left, key_, node)
+            else:  # key == node.key
+                if node.right is None and node.left is None:  # node has no children
+                    if prev_node.right is node:  # "node to be deleted" is on the right
+                        prev_node.right = None
+                    elif prev_node.left is node:  # "node to be deleted is on the left
+                        prev_node.left = None
+                    del node
+                elif node.right is None or node.left is None:  # node has one child
+                    child = node.right if node.right is not None else node.left  # child of the "node to be deleted"
+                    if prev_node.right is node:  # "node to be deleted" is on the right
+                        prev_node.right = child
+                    elif prev_node.left is node:  # "node to be deleted" is on the left
+                        prev_node.left = child
+                    del node
+                else:  # node has both children (left and right)
+                    curr = node.right
+                    prev = node
+                    while curr.left is not None:
+                        prev = curr
+                        curr = curr.left
+                    # change key, value of "node to be deleted" on key, value of the least key in the right tree
+                    node.key = curr.key
+                    node.value = curr.value
+                    del_node(curr, curr.key, prev)  # deletion of node with the least key in the right tree
+
+        if self.root is not None and self.root.right is None and self.root.left is None:
+            del self.root
+            self.root = None
+        del_node(self.root, key)
 
 
 if __name__ == '__main__':
     tree = TreeMap()
-    tree[13] = 'second'
-    print(tree[13], tree.root.value)
-    tree[7] = 'third'
+    # tree[13] = 'second'
+    # print(tree[13], tree.root.value)
+    # tree[7] = 'third'
+    # print(tree[7])
+    tree[10] = 'root'
+    tree[7] = 'left'
+    tree[12] = 'right'
+    tree[9] = 'left->right'
+    tree[8] = 'left->right->left'
+    tree[6] = 'left->left'
+    del tree[9]
+    # print
     # print(tree[7], tree.root.left, tree.root.right)
     # tree[6] = 'forth'
     # print(tree[6], tree.root.left.left.key, tree.root.right)
--- tests/tree_map_tests.py
+++ tests/tree_map_tests.py
@@ -22,6 +22,39 @@ class BinaryTreeTesting(MapTesting, unittest.TestCase):
         self.map['a'] = 1
         self.assertEqual(self.map['a'], self.map.root.left.value)
 
+    def test_del_leaf(self):
+        self.map[5] = 'root'
+        self.map[8] = 'right'
+        self.map[4] = 'left'
+        self.map[12] = 'right->right'
+        del self.map[12]
+        self.assertEqual(self.map.root.right.right, None)
+
+    def test_del_node_with_one_child(self):
+        self.map[33] = 'root'
+        self.map[35] = 'right'
+        self.map[5] = 'left'
+        self.map[17] = 'left->right'
+        self.map[18] = 'left->right->right'
+        self.map[1] = 'left->left'
+        self.map[4] = 'left->left->right'
+        del self.map[17]
+        self.assertEqual(self.map.root.left.right.value, self.map[18])
+        del self.map[1]
+        self.assertEqual(self.map.root.left.left.value, 'left->left->right')
+
+    def test_del_node_with_two_children(self):
+        self.map[10] = 'root'
+        self.map[7] = 'left'
+        self.map[12] = 'right'
+        self.map[9] = 'left->right'
+        self.map[8] = 'left->right->left'
+        self.map[6] = 'left->left'
+        del self.map[7]
+        self.assertEqual(self.map.root.left.value, 'left->right->left')
+        self.assertEqual(self.map.root.left.right.value, 'left->right')
+        self.assertIs(self.map.root.left.right.left, None)
+
 
 if __name__ == '__main__':
     unittest.main()
